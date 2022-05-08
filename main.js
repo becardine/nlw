0e543bb2adbf6ed8ed67ff4ee93bafbdbd634c1b
becardine <brenda_alves@live.com>
2022-05-08 13:46:38 -0300

    nav menu with js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function onScroll() {
-    console.log('mensagem')
+    if(scrollY > 0) {
+        navigation.classList.add('scroll')
+    } else {
+        navigation.classList.remove('scroll')
+    }
+    
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
 }
