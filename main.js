8b6b8d5660797714edf7633f891d956ece9d19be
becardine <brenda_alves@live.com>
2022-05-10 00:52:46 -0300

    adjusts design and responsiveness

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
 function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnSroll()
+}
+
+function showNavOnScroll() {
     if(scrollY > 0) {
         navigation.classList.add('scroll')
     } else {
         navigation.classList.remove('scroll')
     }
-    
 }
+
+function showBackToTopButtonOnSroll() {
+    if (scrollY > 500) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
 
 function openMenu() {
     document.body.classList.add('menu-expanded')
