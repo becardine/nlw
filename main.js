d2303a00ffbceadcd23e18316c70095231887837
becardine <brenda_alves@live.com>
2022-05-10 21:35:08 -0300

    add after in the menu with js

--- main.js
+++ main.js
@@ -5,6 +5,39 @@ onScroll()
 function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnSroll()
+
+    activateMenuCurrentSection(home)
+    activateMenuCurrentSection(services)
+    activateMenuCurrentSection(about)
+    activateMenuCurrentSection(contact)
+}
+
+function activateMenuCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+
+    const sectionEndsAt = sectionTop + sectionHeight
+
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+
+    const sectionId = section.getAttribute('id')
+
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+
+    if(sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
+    
 }
 
 function showNavOnScroll() {
